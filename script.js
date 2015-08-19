77baf65fea29e083f8cfb14221cc5f7d499fc838
Eunice Kim <ekimed@gmail.com>
2015-08-18 22:18:02 -0700

    added error handling and functionality

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-angular.module('waitstaffCalc', [])
+angular.module('waitstaffCalc', ['ngMessages'])
 	.controller('myCtrl', function ($scope) {
 		// why can't I assign scope to a variable
 		// var basePrice = $scope.basePrice;
@@ -30,22 +30,29 @@ angular.module('waitstaffCalc', [])
 		setInitValues();
 		
 		$scope.submit = function (form) {
-			// customer charges update
-			$scope.subtotal = $scope.meal.basePrice + $scope.meal.basePrice * ($scope.meal.taxRate/100);
-			$scope.tip = $scope.meal.basePrice * ($scope.meal.tipPercent/100);
-			$scope.total = $scope.subtotal + $scope.tip;
+			if (form.$valid) {
+				// customer charges update
+				$scope.subtotal = $scope.meal.basePrice + $scope.meal.basePrice * ($scope.meal.taxRate/100);
+				$scope.tip = $scope.meal.basePrice * ($scope.meal.tipPercent/100);
+				$scope.total = $scope.subtotal + $scope.tip;
 
-			// my earnings info update
-			$scope.earnInfo.tipTotal = $scope.earnInfo.tipTotal + $scope.tip;
-			$scope.earnInfo.mealCount ++;
-			$scope.earnInfo.avgTipMeal = $scope.earnInfo.tipTotal / $scope.earnInfo.mealCount;
+				// my earnings info update
+				$scope.earnInfo.tipTotal = $scope.earnInfo.tipTotal + $scope.tip;
+				$scope.earnInfo.mealCount ++;
+				$scope.earnInfo.avgTipMeal = $scope.earnInfo.tipTotal / $scope.earnInfo.mealCount;
 
-			console.log(defaultEarnings)
-			
-			// reset the form
-			$scope.mealDetails.$setPristine();
-			resetForm();
+				console.log(defaultEarnings)
+
+				
+				// reset the form
+				$scope.mealDetails.$setPristine();
+				resetForm();
+			}
 		};
+
+		$scope.cancel = function () {
+			resetForm();
+		}
 
 		$scope.resetAll = function () {
 			$scope.earnInfo = defaultEarnings;
